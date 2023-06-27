de89425712478f10a4418a1f72b72e98af173614
julianpins <julian.pins@mailbox.org>
2023-06-27 08:46:29 +0200

    added negative values and inf, nan

--- Implementierung/approxArsinh_lookup2.c
+++ Implementierung/approxArsinh_lookup2.c
@@ -2,6 +2,9 @@
 #include <string.h>
 
 double approxArsinh_lookup2 (double x){
+    if(x == -INFINITY || x == INFINITY || x == NAN || x == -NAN){
+        return x;
+    }
     signed long long mask = 0x7FFF000000000000;
     signed long long xToBit;
     memcpy(&xToBit, &x, 8);
@@ -9,6 +12,6 @@ double approxArsinh_lookup2 (double x){
     double ylow = table[Index];
     double yhigh = table[Index+1];
     double relation = (double)(xToBit & 0xFFFFFFFFFFFF) / (double) 0x1000000000000;
-    return ylow + relation * (yhigh - ylow);
-
+    double result = ylow + relation * (yhigh - ylow);
+    return x>=0 ? result : -result;
 }
