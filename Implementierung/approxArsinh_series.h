e5926d9ef3ab50aaa836067fc4ec6ca63132f264
Ludwig Groeber <ludwig.groeber@tum.de>
2023-07-13 11:43:21 +0200

    Refactoring and reformatting

--- Implementierung/approxArsinh_series.h
+++ Implementierung/approxArsinh_series.h
@@ -23,8 +23,8 @@ static double const coeffs_below[] = {1.0, -0.16666666666666666, 0.075, -0.04464
                                       0.0010211486797106276, -0.0009860731369833314, 0.0009529606197429565,
                                       -0.0009216606921836336, 0.0008920374230917097, -0.0008639677124658674,
                                       0.0008373398416027121, -0.0008120522129086703, 0.0007880122513582056,
-                                      -0.000765135444137165}; 
-    //stores precalculated coefficients for the Taylor series of arsinh around 0
+                                      -0.000765135444137165};
+//stores precalculated coefficients for the Taylor series of arsinh around 0
 
 static double const coeffs_taylor[] = {1.0, -0.5, 0.3333333333333333, -0.25, 0.2, -0.16666666666666666,
                                        0.14285714285714285, -0.125, 0.1111111111111111, -0.1, 0.09090909090909091,
@@ -39,8 +39,8 @@ static double const coeffs_taylor[] = {1.0, -0.5, 0.3333333333333333, -0.25, 0.2
                                        0.024390243902439025, -0.023809523809523808, 0.023255813953488372,
                                        -0.022727272727272728, 0.022222222222222223, -0.021739130434782608,
                                        0.02127659574468085, -0.020833333333333332, 0.02040816326530612, -0.02,
-                                       0.0196078431372549}; 
-    //stores precalculated coefficients of the Taylor series of the ln around 1
+                                       0.0196078431372549};
+//stores precalculated coefficients of the Taylor series of the ln around 1
 
 static double const coeffs_rest[] = {0.25, -0.09375, 0.052083333333333336, -0.0341796875, 0.024609375, -0.018798828125,
                                      0.014962332589285714, -0.012273788452148438, 0.010303921169704862,
@@ -58,4 +58,4 @@ static double const coeffs_rest[] = {0.25, -0.09375, 0.052083333333333336, -0.03
                                      0.0009975391036924398, -0.000963789717694581, 0.0009319013665412294,
                                      -0.0009017334820383588, 0.0008731588583431641, -0.0008460621316194071,
                                      0.0008203384599791669, -0.0007958923738717878, 0.0007726367720208628};
-    //stores precalculated coefficients of the asymptotic expansion rest series
+//stores precalculated coefficients of the asymptotic expansion rest series
