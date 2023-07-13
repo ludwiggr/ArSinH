cbe61a0eb6304b99e603dafc2d23c4c528574e78
Ludwig Groeber <ludwig.groeber@tum.de>
2023-07-13 11:47:10 +0200

    new Links to new Files, Reformatting, Code Comments

--- Implementierung/approxArsinh_lookup.c
+++ Implementierung/approxArsinh_lookup.c
@@ -1,17 +1,20 @@
 #include "approxArsinh_lookup.h"
-#include <string.h>
 
+/* An Approximation of the arsinh using a lookup-table
+ *
+ * Returns: An Approximation of the arsinh(x)
+ */
 double approxArsinh_lookup(double x) {
-    if (x == -INFINITY || x == INFINITY || x == NAN || x == -NAN) { //catch edgecases
+    if (x == -INFINITY || x == INFINITY || x == NAN || x == -NAN) {
         return x;
-    }     
-    signed long long mask = 0x7FFF000000000000;                     
+    }
+    signed long long mask = 0x7FFF000000000000;
     signed long long xToBit;
-    memcpy(&xToBit, &x, 8);     //get exponent and first 4 bits of mantissa
-    signed long long Index = (xToBit & mask) >> 48;      //as table index
-    double ylow = table[Index];                         //table entry before x
-    double yhigh = table[Index + 1];                    //table entry after x
-    double relation = (double) (xToBit & 0xFFFFFFFFFFFF) / (double) 0x1000000000000; 
-    double result = ylow + relation * (yhigh - ylow);  //use linear interpolation to approximate
-    return x >= 0 ? result : -result;               //use point symmetry for negative values
+    memcpy(&xToBit, &x, 8);                                 //Bithack: get exponent and first 4 bits of mantissa
+    signed long long index = (xToBit & mask) >> 48;         //table index
+    double ylow = table[index];
+    double yhigh = table[index + 1];
+    double relation = (double) (xToBit & 0xFFFFFFFFFFFF) / (double) 0x1000000000000;
+    double result = ylow + relation * (yhigh - ylow);
+    return x >= 0 ? result : -result;
 }
