f6d718407c773d296426160c5d9756c7fb26d216
Ludwig Groeber <ludwig.groeber@tum.de>
2023-07-13 11:40:31 +0200

    Comments and Refactoring accuracy_test.c

--- Implementierung/accuracy_test.c
+++ Implementierung/accuracy_test.c
@@ -1,15 +1,13 @@
-#include "approxArsinh_lookup.h"
 #include <stdio.h>
-#include "approxArsinh_series.h"
 #include <math.h>
+#include "approxArsinh_lookup.h"
+#include "approxArsinh_series.h"
+#include "approxArsinh_complexInstructions.h"
 
-double approxArsinh_predefined(double x) {
-    if (x > 1e16) {
-        return log(2) + log(x);
-    }
-    return log(x + sqrt(x * x + 1));
-}
-
+/* Calls the Accuracy Tests and returns the measured Values
+ *
+ * Returns: The accuracy test results
+ */
 int main() {
 
     FILE *fp1, *fp2, *fp3, *fp4, *fp5;
@@ -19,13 +17,13 @@ int main() {
     fp4 = fopen("predefined.txt", "w+");
     fp5 = fopen("differentSeries.txt", "w+");
 
-    for(int i = -1000; i<1024; i++){
+    for (int i = -1000; i < 1024; i++) {
         double b = pow(2, i);
-        for(int j = 0; j<20; j++){
-            double x = b + j * (b/20);
+        for (int j = 0; j < 20; j++) {
+            double x = b + j * (b / 20);
             double lookup = approxArsinh_lookup(x);
             double series = approxArsinh_series(x);
-            double predefined = approxArsinh_predefined(x);
+            double predefined = approxArsinh_complexInstructions(x);
             double diff = approxArsinh_differentSeries(x);
             fprintf(fp1, "%.15g, ", lookup);
             fprintf(fp2, "%.15g, ", series);
@@ -34,16 +32,4 @@ int main() {
             fprintf(fp5, "%.15g, ", diff);
         }
     }
-
-    /*double m = 1.3;
-    while (x < 1e300) {
-        double lookup = approxArsinh_lookup(x);
-        double series = approxArsinh_series(x);
-        double predefined = approxArsinh_predefined(x);
-        fprintf(fp1, "%.*f, ", 15, lookup);
-        fprintf(fp2, "%.*f, ", 15, series);
-        fprintf(fp3, "%.*f, ", 15, x);
-        fprintf(fp4, "%.*f, ", 15, predefined);
-        x = x * m;
-    }*/
 }
