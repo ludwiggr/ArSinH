0bf0a9a43daacc5b52a82b316d2905f9c6ac103a
Ludwig Groeber <ludwig.groeber@tum.de>
2023-07-14 23:30:07 +0200

    Rechtschreibung, Formatierung und vereinheitlichung Kommentare approxArsinh_series.c

--- Implementierung/approxArsinh_series.c
+++ Implementierung/approxArsinh_series.c
@@ -1,21 +1,21 @@
 #include "approxArsinh_series.h"
 
-/* Non-changing global variable for log_2 */
+// Non-changing global variable for log_2
 static const double LOG_TWO = 0.69314718055994530941;
 
 
-/* Series Approximation of arsinh for [-1; +1]
- * using the Taylorseries around 0
+/*
+ * Series Approximation of arsinh for [-1; +1] using the Taylor series around 0
  *
- * Input: |x| < 1
+ * Arguments: |x| < 1
  * 
  * Features: fixed number of series terms to avoid case distinctions
  * x^2 as base for terms of the series
- * lookuptable for coefficents and horner scheme to optimize calculation of series terms
+ * lookup-table for coefficients and horner scheme to optimize calculation of series terms
  *
  * Returns: An approximation of arsinh for [-1 ; +1]
  * exact results for |x| < 0.25
- * exponentielly less accurate results for |x| closer to 1
+ * exponentially less accurate results for |x| closer to 1
  */
 double approxArsinh_series(double x) {
     size_t iterations = 13;
@@ -30,15 +30,16 @@ double approxArsinh_series(double x) {
 }
 
 
-/* Approximation of the Remainder of the Asymptotic Expansion
+/*
+ * Approximation of the Remainder of the Asymptotic Expansion
  *
- * Input: x>1
+ * Arguments: x>1
  * 
  * Features: Fixed number of calculated series terms -> No case distinction
  * Calculates 13 terms of the series, to ensure exact results for |x|>4
- * lookuptable for coefficents and horner scheme to optimize calculation of series terms
+ * lookup-table for coefficients and horner scheme to optimize calculation of series terms
  * 
- * Returns: exact value of restseries for |x|>4 
+ * Returns: exact value of rest-series for |x|>4
  * Exponentially less accurate results, the closer x is to 1
  */
 double approxAsymptoticExpansionRest(double x) {
@@ -53,17 +54,16 @@ double approxAsymptoticExpansionRest(double x) {
     return sum;
 }
 
-/* Computes the Taylor Series for ln(x) around 1
-* applicable for 0<|x|<=2 
+/*
+ * Computes the Taylor Series for ln(x) around 1, applicable for 0<|x|<=2
  *
- * Input: 0.667 < x < 1.333
+ * Arguments: 0.667 < x < 1.333
  * 
  * Features: Fixed number of calculated series terms -> No case distinction
  * Since -0.33<=fak<=0.33, after at most 27 terms of the series, the terms don't matter for the end result, due to floating point cancellation
- * lookuptable for coefficents and horner scheme to optimize calculation of series terms
-
+ * lookup-table for coefficients and horner scheme to optimize calculation of series terms
  * 
- * Returns: exact calue of ln(x) for input values
+ * Returns: exact value of ln(x) for input values
  */
 double approxLnTaylor(double x) {  //
     double fak = x - 1;
@@ -78,13 +78,14 @@ double approxLnTaylor(double x) {  //
 }
 
 
-/* Converts ln(x) to ln(x'*2^n) = ln(x')+n*ln(2) so that the Taylor series can be applied to x'
+/*
+ * Converts ln(x) to ln(x'*2^n) = ln(x')+n*ln(2) so that the Taylor series can be applied to x'
  *
- * Input: x > 1
+ * Arguments: x > 1
  * 
  * Features: Bitmask, to get exponent and mantissa
- *  optimization to ensure input for the Taylor series is as close to 1 as possible
- *  calls TaylorSeries to calculate ln(mantissa)
+ * optimization to ensure input for the Taylor series is as close to 1 as possible
+ * calls TaylorSeries to calculate ln(mantissa)
  *
  * Returns: ln(x) = ln(mantissa * 2^exp) = ln(mantissa) + exp * ln(2)
  */
@@ -97,18 +98,18 @@ double approxLn(double x) {
     double mantissa;
     memcpy(&mantissa, &xLowerTwo, 8);
     /*ensures input is as close to one as possible
-     *so Taylor series returns more accurate results
-     */
-    if (mantissa > 1.3333333333333333) { 
-        mantissa /= 2;                   
+     *so Taylor series returns more accurate results */
+    if (mantissa > 1.3333333333333333) {
+        mantissa /= 2;
         exp++;
     }
     return approxLnTaylor(mantissa) + exp * LOG_TWO;
 }
 
-/* Uses the approximation arsinh(x) = ln(2x) + error(x)
+/*
+ * Uses the approximation arsinh(x) = ln(2x) + error(x)
  *
- * Input: |x| > 1
+ * Arguments: |x| > 1
  * 
  * Features: Uses symmetry of the arsinh function
  * already converts ln(2x) to ln(2)+ln(x)
@@ -126,13 +127,13 @@ double approxXAboveOne(double x) {
 }
 
 
-
-/* Series Approximation of arsinh for [-inf ; +inf]
+/*
+ * Series Approximation of arsinh for [-inf ; +inf]
  *
- * Input: any double
+ * Arguments: any double x
  * 
  * Features: Case distinction for better series Approximation
- *  Uses TaylorSeries of arsinh(x) for |x|<1 and ln(2x)+error(x) for |x|>1 
+ * Uses TaylorSeries of arsinh(x) for |x|<1 and ln(2x)+error(x) for |x|>1
  *
  * Returns: An approximation of arsinh for [-inf ; +inf]
  * exact results outside the interval 0.25<|x|<4
