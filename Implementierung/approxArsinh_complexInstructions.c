72cd7914efab964d62802089bd71eab7ae1ee789
Ludwig Groeber <ludwig.groeber@tum.de>
2023-07-14 23:30:39 +0200

    Rechtschreibung, Formatierung und vereinheitlichung Kommentare approxArsinh_complexInstructions.c

--- Implementierung/approxArsinh_complexInstructions.c
+++ Implementierung/approxArsinh_complexInstructions.c
@@ -1,12 +1,13 @@
 #include "approxArsinh_complexInstructions.h"
 
-/* Calls the pre-defined Functions log and sqrt in <math.h> to compute the arsinh
+/*
+ * Calls the pre-defined Functions log and sqrt in <math.h> to compute the arsinh
  * Uses approximation ln(2x) for greater x-values to avoid overflows
  *
- * Input: any double
+ * Arguments: any double x
  * 
  * Returns: an exact result with maximum double accuracy for |x| > 2^-26
- *  inaccurate values for |x| < 2^-26 due to floating point cancellation
+ * inaccurate values for |x| < 2^-26 due to floating point cancellation
  */
 double approxArsinh_complexInstructions(double x) {
     if (x > 1e16) {
